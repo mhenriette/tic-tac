ed2dc702284d2ff66a64fbf63718ceb19241c8bc
mhenriette <mhenriette00@gnmail.com>
2023-03-17 18:23:08 +0200

    initial installation of react and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="App" >
+      <h1 className='text-red-500'>Hello world for testing</h1>
+
+    </div >
+  )
+}
+
+export default App
